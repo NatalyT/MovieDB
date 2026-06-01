9fdb17f82ab2a278175f3649e2d2b04bdba4a734
Nataly Tatarintseva <ngranetchi@gmail.com>
2026-06-01 11:38:03 +0200

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Natalia Tatarinteva on 01.06.26.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
